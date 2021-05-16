0d2d3aa8129baf38f3943e00953b5050e4256011
Sai <saivinni4@gmail.com>
2021-05-16 16:58:32 +0530

    cucumber bdd parallel execution first git commit

---
+++ test-output-thread/report.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "f570d1c3-12bd-4b88-a91a-6e4e41bdbf78",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1611495247729,
+    "group": 18,
+    "content": "",
+    "tags": "",
+    "end": 1611495334619,
+    "className": "passed"
+  },
+  {
+    "id": "964adf18-2d80-4ec4-94ac-533e770d963f",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts page title",
+    "start": 1611495247730,
+    "group": 14,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1611495324952,
+    "className": "failed"
+  },
+  {
+    "id": "d62d14f7-be8e-4c4b-801f-d14139efa67d",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1611495247731,
+    "group": 16,
+    "content": "",
+    "tags": "",
+    "end": 1611495370021,
+    "className": "passed"
+  },
+  {
+    "id": "41b3cf8c-4676-42dd-9271-f9fbe85779bd",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts section count",
+    "start": 1611495247733,
+    "group": 15,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1611495328461,
+    "className": "passed"
+  },
+  {
+    "id": "944a484d-7b99-46f2-abf5-59b7277838b2",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password link",
+    "start": 1611495247581,
+    "group": 17,
+    "content": "",
+    "tags": "",
+    "end": 1611495323095,
+    "className": "failed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 14,
+    "content": "Thread[TestNG-PoolService-0,5,main]"
+  },
+  {
+    "id": 15,
+    "content": "Thread[TestNG-PoolService-1,5,main]"
+  },
+  {
+    "id": 16,
+    "content": "Thread[TestNG-PoolService-2,5,main]"
+  },
+  {
+    "id": 17,
+    "content": "Thread[TestNG-PoolService-3,5,main]"
+  },
+  {
+    "id": 18,
+    "content": "Thread[TestNG-PoolService-4,5,main]"
+  }
+]);
+});
